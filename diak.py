27f6e6bfb67f453b6ff8a73b668259c0266283d1
k.gyuszi <kovacsgy.24d@acsjszki.hu>
2026-02-19 12:18:49 +0100

    jav

--- diak.py
+++ diak.py
@@ -1,3 +1,4 @@
+# 1. Fájl beolvasása
 fajl = open("diak.txt", "r", encoding="utf-8")
 diakok = []
 
@@ -10,8 +11,7 @@ fajl.close()
 
 print("Diákok száma:", len(diakok))
 
-# 2. Legmagasabb diák 
-
+# 2. Legmagasabb diák keresése
 legmag = 0
 legnev = ""
 for diak in diakok:
@@ -21,16 +21,11 @@ for diak in diakok:
 
 print("Legmagasabb:", legnev, "-", legmag, "cm")
 
-# 3. Rendezés csökkenő sorrendben 
-
+# 3. Rendezés 
 diakok.sort(key=lambda x: x[1], reverse=True)
 
 print("Tornasor:")
 for diak in diakok:
     print(diak[0], diak[1])
 
-
-fajl = open("rendezve.txt", "w", encoding="utf-8")
-for diak in diakok:
-    fajl.write(diak[0] + " " + str(diak[1]) + "\n")
 fajl.close()
